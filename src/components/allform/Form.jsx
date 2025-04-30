1b045b25030cb4c2ac9b9560b49ed8e0dc74290c
AmarMauryaDeveloper <amarmaurya296@gamil.com>
2025-04-30 13:44:07 +0530

    save

--- src/components/allform/Form.jsx
+++ src/components/allform/Form.jsx
@@ -125,7 +125,7 @@ export default function Form() {
 
   return (
     <div className="relative w-full px-4 sm:px-6 md:px-4 pb-3 border-3 border-white rounded-lg">
-      <div className="my-4 text-[35px] tracking-tight text-[#dba577] font-extrabold">
+      <div className="my-4 text-[31px] tracking-tight text-[#dba577] font-extrabold">
         Kindly, Fill the Form :
       </div>
 
