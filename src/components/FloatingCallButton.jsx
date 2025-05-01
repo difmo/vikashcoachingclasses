23fdee2ec77e9a798e2ab6f181710b8efcb4de87
AmarMauryaDeveloper <amarmaurya296@gamil.com>
2025-05-01 22:25:14 +0530

    dfd

--- src/components/FloatingCallButton.jsx
+++ src/components/FloatingCallButton.jsx
@@ -19,7 +19,7 @@ const FloatingCallAndWhatsappButtons = () => {
           ease: "easeInOut",
         }}
       >
-        <img src={whatsapp} className="h-14 w-14 rounded-full" />
+        <img src={whatsapp} className="h-10 w-10 rounded-full" />
       </motion.a>
     </div>
   );
