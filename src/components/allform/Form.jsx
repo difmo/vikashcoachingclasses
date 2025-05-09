43c53bab6dc2301165e07e115ee03926c4445ef3
AmarMauryaDeveloper <amarmaurya296@gamil.com>
2025-05-09 21:22:24 +0530

    save

--- src/components/allform/Form.jsx
+++ src/components/allform/Form.jsx
@@ -147,7 +147,7 @@ export default function Form() {
     try {
       await addDoc(collection(db, "Vikasrequests"), {
         name: "Ravi Kumar",
-        phone: "+918853389395",
+        phone: "8853389395",
         classType: "Online",
         level: "Senior",
         board: "CBSE",
