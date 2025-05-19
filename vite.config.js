7acc101034d4bdd467d8dd680d66d8fd8cf1807b
7844Pritam <pritamsharma7844@gmail.com>
2025-05-19 14:00:38 +0530

    save

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    // Increase chunk size warning limit (default is 500 kB)
+    chunkSizeWarningLimit: 1500, // You can adjust as needed (in KB)
+
+    // Optional: Enable manual code splitting
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("react")) return "react";
+            if (id.includes("tailwindcss")) return "tailwind";
+            return "vendor"; // other libraries
+          }
+        },
+      },
+    },
+  },
 });
